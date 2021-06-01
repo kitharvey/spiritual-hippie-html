2d2bf791a91d9f82677b2cd28a5e1311bfb51675
Kit Harvey V. Caubalejo <54163221+kitharvey@users.noreply.github.com>
2021-06-01 20:08:57 +0800

    fix mobile

--- script.js
+++ script.js
@@ -5,15 +5,15 @@ const paymentBTN = document.getElementById('payment-button')
 const continuePaymentBTN = document.getElementById('continue-to-payment')
 const completeOrderBTN = document.getElementById('complete-order')
 const modalDOM = document.getElementById('modal')
+const orderSummaryBTN = document.getElementById('order-summary-button')
+const orderWrapperDOM = document.getElementById('order-wrapper')
+const orderSummaryBTNTITLE = document.getElementById('order-summary-button-title')
+const orderSummaryBTNARROW = document.getElementById('order-summary-button-arrow')
 const closeModalBTN = document.querySelectorAll('#close-modal')
 
-const onLoad = () => {
-    shippingDOM.style.display = 'block'
-    paymentDOM.style.display = 'none'
-    modalDOM.style.display = 'none'
-    shippingBTN.classList.replace('idle', 'active')
-    paymentBTN.classList.replace('active', 'idle')
 
+const onLoad = () => {
+    let isOrderSummaryOpen = true
     function onclickShippingBTN() {
         shippingDOM.style.display = 'block'
         paymentDOM.style.display = 'none'
@@ -27,9 +27,26 @@ const onLoad = () => {
         paymentBTN.classList.replace('idle', 'active')
     }
 
+    function onClickOrderSummary(){
+        isOrderSummaryOpen = !isOrderSummaryOpen
+
+        if(isOrderSummaryOpen){
+            orderWrapperDOM.style.height = 'max-content'
+            orderSummaryBTNTITLE.textContent = 'Hide order summary'
+            orderSummaryBTNARROW.style.transform = 'rotate(180deg)'
+        }
+        else{
+            orderWrapperDOM.style.height = '0px'
+            orderSummaryBTNTITLE.textContent = 'Show order summary'
+            orderSummaryBTNARROW.style.transform = 'rotate(0deg)'
+        }
+        
+    }
+
     paymentBTN.addEventListener('click', onclickPaymentBTN)
     shippingBTN.addEventListener('click', onclickShippingBTN)
     continuePaymentBTN.addEventListener('click', onclickPaymentBTN)
+    orderSummaryBTN.addEventListener('click', onClickOrderSummary)
     completeOrderBTN.addEventListener('click', () => modalDOM.style.display = 'flex')
     closeModalBTN[0].addEventListener('click', () => modalDOM.style.display = 'none')
     closeModalBTN[1].addEventListener('click', () => modalDOM.style.display = 'none')
