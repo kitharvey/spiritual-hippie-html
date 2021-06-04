38f2855a673edc2be4ca0818956adf166d68b441
Kit Harvey V. Caubalejo <54163221+kitharvey@users.noreply.github.com>
2021-06-04 17:47:39 +0800

    add functions and fixed design issues

--- script/index.js
+++ script/index.js
@@ -7,10 +7,12 @@ const completeOrderBTN = document.getElementById('complete-order')
 const modalDOM = document.getElementById('modal')
 const orderSummaryBTN = document.getElementById('order-summary-button')
 const orderWrapperDOM = document.getElementById('order-wrapper')
+const productWrapperDOM = document.getElementById('product-wrapper')
 const orderSummaryBTNTITLE = document.getElementById('order-summary-button-title')
 const orderSummaryBTNARROW = document.getElementById('order-summary-button-arrow')
 const differentAddressBTN = document.getElementById('different-address')
 const sameAddressBTN = document.getElementById('same-address')
+const securedShippingAddressBTN = document.getElementById('secured-shipping')
 const differentAddressFORM = document.getElementById('different-address-form')
 const paymentCCBTN = document.getElementById('creditcard-button')
 const paymentPPBTN = document.getElementById('paypal-button')
@@ -18,9 +20,95 @@ const paymentCCFORM = document.getElementById('payment-cc-input')
 const paymentPPFORM = document.getElementById('payment-pp-info')
 const closeModalBTN = document.querySelectorAll('#close-modal')
 
+const productData = [
+    {
+      img: "/assets/shoe3-1.png",
+      name: "Yin Yang Mandala",
+      description: "Women's Aqua Barefoot Shoes",
+      details: ["Black Sole", "US 9-10", "EU40-41"],
+      price: 35,
+      qnty: 1,
+      isFree: true,
+    },
+    {
+      img: "/assets/shoe2-1.png",
+      name: "Yin Yang Mandala",
+      description: "Women's Aqua Barefoot Shoes",
+      details: ["Black Sole", "US 9-10", "EU40-41"],
+      price: 35,
+      qnty: 1,
+      isFree: false,
+    },
+    {
+      img: "/assets/shoe1-1.png",
+      name: "Watercolor Flowers",
+      description: "Women's Aqua Barefoot Shoes",
+      details: ["Black Sole", "US 9-10", "EU40-41"],
+      price: 35,
+      qnty: 2,
+      isFree: false,
+    },
+    {
+      img: "/assets/GiftYWV1.png",
+      name: "Copy of Free Mystery Gift",
+      details: ["Free Gift", "Vibrant Vibes", "One Size"],
+      price: 0,
+      qnty: 1,
+      isFree: false,
+    },
+    {
+      img: "/assets/SafeGuard.png",
+      name: "Safeguard - Shipping Guarantee",
+      description: "Safeguard shipping guarantee",
+      details: [".98"],
+      price: 0.98,
+      qnty: 1,
+      isFree: false,
+    },
+  ]
+
+
+sameAddressBTN.checked = true
+securedShippingAddressBTN.checked = true
+paymentCCBTN.checked = true
 
 const onLoad = () => {
     let isOrderSummaryOpen = true
+    let isSecured = true
+    let products = ''
+
+
+
+    productData.forEach( 
+        ({img,
+        name,
+        description,
+        details,
+        price,
+        qnty,
+        isFree}) => {
+        products += `<div class="product" id=${name}>
+                        <div class="img-wrapper">
+                            <img src=${img} alt=${name} />
+                            <span class="qnty">${qnty}</span>
+                        </div>
+                    
+                        <div class="info">
+                            <p class="name">${name}</p>
+                            ${description ? `<p class="description">${description}</p>` : ""}
+                            <div class="details">
+                                ${details.map( detail => `<p>${detail}</p>` ).join("")}
+                            </div>
+                        </div>
+                        <div class="price-wrapper">
+                        <p class=${isFree ? "no-price" : "price"}>$${price.toFixed(2)}</p>
+                        ${isFree ? "<p class='free'>FREE</p>" : ""}
+                        </div>
+                    </div>`
+    })
+
+    productWrapperDOM.innerHTML = products
+
     function onclickShippingBTN() {
         shippingDOM.style.display = 'block'
         paymentDOM.style.display = 'none'
@@ -50,6 +138,8 @@ const onLoad = () => {
         
     }
 
+
+
     function selectBillingAddress(){
         if(differentAddressBTN.checked) {
             differentAddressFORM.style.display = 'block'
@@ -69,6 +159,39 @@ const onLoad = () => {
             paymentPPFORM.style.display = 'flex'
         }
     }
+
+    function securedShippingAddress(){
+        isSecured = !isSecured
+        securedShippingAddressBTN.checked = isSecured
+        if(!isSecured){
+            const safeguard = document.getElementById('Safeguard')
+            productWrapperDOM.removeChild(safeguard)
+        }
+        if(isSecured){
+            const safeguard = document.createElement('div')
+            safeguard.className = "product" 
+            safeguard.id = "Safeguard"
+            safeguard.innerHTML = `<div class="img-wrapper">
+                                <img src="/assets/SafeGuard.png" alt="Safeguard - Shipping Guarantee" />
+                                <span class="qnty">1</span>
+                            </div>
+                        
+                            <div class="info">
+                                <p class="name">Safeguard - Shipping Guarantee</p>
+                                <p class="description">Safeguard shipping guarantee</p>
+                                <div class="details">
+                                   <p>.98</p>
+                                </div>
+                            </div>
+                            <div class="price-wrapper">
+                                <p class="price">$0.98</p>
+                            </div>
+                        </div>`
+            productWrapperDOM.appendChild(safeguard)
+        }
+    }
+
+    securedShippingAddressBTN.onclick = securedShippingAddress
 
     differentAddressBTN.onclick = selectBillingAddress
     sameAddressBTN.onclick = selectBillingAddress
